03cdbe275f5c7630af53da0188cf11e03377bfce
abdumovi <abdumovi@gmail.com>
2021-08-23 22:10:25 +0500

    slider_add

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.our-clients').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  dots: true,
+  infinite: true
+});
+
+$('.carousel-div').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  infinite: true
+});
